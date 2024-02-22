e2dc6b4ade905c3e7b6ec527c2f849cc5ce5d8db
lacmc <lacfg@163.com>
2024-02-22 16:03:50 +0800

    add

--- base-example/src/main/java/io/github/chaogeoop/base/example/app/controllers/CommonCountController.java
+++ base-example/src/main/java/io/github/chaogeoop/base/example/app/controllers/CommonCountController.java
@@ -1,6 +1,7 @@
 package io.github.chaogeoop.base.example.app.controllers;
 
 import io.github.chaogeoop.base.business.common.CommonCountProvider;
+import io.github.chaogeoop.base.business.common.annotations.UserInfo;
 import io.github.chaogeoop.base.business.mongodb.MongoPersistEntity;
 import io.github.chaogeoop.base.business.mongodb.PersistProvider;
 import io.github.chaogeoop.base.business.redis.KeyEntity;
@@ -12,6 +13,7 @@ import io.github.chaogeoop.base.business.common.helpers.JsonHelper;
 import io.github.chaogeoop.base.example.app.keyregisters.CommonCountKeyRegister;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import io.github.chaogeoop.base.example.repository.entities.UserContext;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.tuple.Pair;
@@ -33,7 +35,9 @@ public class CommonCountController {
     private RedisProvider redisProvider;
 
     @PostMapping("/upload")
-    public HttpResult<Boolean> upload(@RequestBody BizInput input) {
+    public HttpResult<Boolean> upload(@RequestBody BookInput input) {
+        Set<CommonCountProvider.CountBiz> countBizList = CollectionHelper.map(input.getBookIds(), o -> getBookFavoriteBiz(o, input.getAction()));
+
         Random rand = new Random();
 
         List<Map<CommonCountProvider.CountBizDate, Long>> bizDateIncMapList = new ArrayList<>();
@@ -43,7 +47,7 @@ public class CommonCountController {
 
             Map<CommonCountProvider.CountBizDate, Long> map = new HashMap<>();
             for (String date : input.getDates()) {
-                for (CommonCountProvider.CountBiz biz : input.getBizList()) {
+                for (CommonCountProvider.CountBiz biz : countBizList) {
                     CommonCountProvider.CountBizDate bizDate = biz.convertToBizDate(date);
 
                     map.put(bizDate, (long) inc);
@@ -63,10 +67,12 @@ public class CommonCountController {
     }
 
     @PostMapping("/getTotalMap")
-    public HttpResult<Map<String, Long>> getTotalMap(@RequestBody BizInput input) {
+    public HttpResult<Map<String, Long>> getTotalMap(@RequestBody BookInput input) {
+        Set<CommonCountProvider.CountBiz> countBizList = CollectionHelper.map(input.getBookIds(), o -> getBookFavoriteBiz(o, input.getAction()));
+
         Map<String, Long> result = new HashMap<>();
 
-        Map<CommonCountProvider.CountBiz, Long> map = this.commonCountProvider.getBizTotalMap(Sets.newHashSet(input.getBizList()));
+        Map<CommonCountProvider.CountBiz, Long> map = this.commonCountProvider.getBizTotalMap(countBizList);
         for (Map.Entry<CommonCountProvider.CountBiz, Long> entry : map.entrySet()) {
             result.put(JsonHelper.writeValueAsString(entry.getKey()), entry.getValue());
         }
@@ -75,12 +81,12 @@ public class CommonCountController {
     }
 
     @PostMapping("/getDateMap")
-    public HttpResult<Map<String, Map<String, Long>>> getDateMap(@RequestBody BizInput input) {
+    public HttpResult<Map<String, Map<String, Long>>> getDateMap(@RequestBody BookInput input) {
+        Set<CommonCountProvider.CountBiz> countBizList = CollectionHelper.map(input.getBookIds(), o -> getBookFavoriteBiz(o, input.getAction()));
+
         Map<String, Map<String, Long>> result = new HashMap<>();
 
-        Map<CommonCountProvider.CountBiz, Map<String, Long>> map = this.commonCountProvider.getBizLogsMap(Sets.newHashSet(input.getBizList()),
-                input.getDates()
-        );
+        Map<CommonCountProvider.CountBiz, Map<String, Long>> map = this.commonCountProvider.getBizLogsMap(countBizList, input.getDates());
 
         for (Map.Entry<CommonCountProvider.CountBiz, Map<String, Long>> entry : map.entrySet()) {
             result.put(JsonHelper.writeValueAsString(entry.getKey()), entry.getValue());
@@ -90,13 +96,15 @@ public class CommonCountController {
     }
 
     @PostMapping("/getCacheExistMap")
-    public HttpResult<Map<String, Map<String, Boolean>>> getCacheExistMap(@RequestBody BizInput input) {
+    public HttpResult<Map<String, Map<String, Boolean>>> getCacheExistMap(@RequestBody BookInput input) {
+        Set<CommonCountProvider.CountBiz> countBizList = CollectionHelper.map(input.getBookIds(), o -> getBookFavoriteBiz(o, input.getAction()));
+
         Map<String, Map<String, Boolean>> map = new HashMap<>();
 
         List<String> dates = CollectionHelper.unique(input.getDates());
         dates.sort(Comparator.comparing(o -> DateHelper.parseStringDate(o, DateHelper.DateFormatEnum.fullUntilDay)));
 
-        for (CommonCountProvider.CountBiz biz : input.getBizList()) {
+        for (CommonCountProvider.CountBiz biz : countBizList) {
             map.put(JsonHelper.writeValueAsString(biz), new LinkedHashMap<>());
             for (String date : dates) {
                 CommonCountProvider.CountBizDate bizDate = biz.convertToBizDate(date);
@@ -125,24 +133,34 @@ public class CommonCountController {
 
 
     @PostMapping("/distributeSafeInc")
-    public HttpResult<Map<String, Long>> distributeSafeInc(@RequestBody BizInput input) {
+    public HttpResult<Map<String, Long>> distributeSafeInc(@UserInfo UserContext userContext, @RequestBody BookInput input) {
+        Set<CommonCountProvider.CountBiz> userCountBizList = CollectionHelper.map(input.getBookIds(), o -> getUserBookFavoriteBiz(userContext, o, input.getAction()));
+        Set<CommonCountProvider.CountBiz> pubCountBizList = CollectionHelper.map(input.getBookIds(), o -> getBookFavoriteBiz(o, input.getAction()));
+
         Map<String, Long> result = new HashMap<>();
 
-
-        Map<CommonCountProvider.CountBiz, Long> incMap = new HashMap<>();
-        for (CommonCountProvider.CountBiz countBiz : input.getBizList()) {
-            incMap.put(countBiz, 1L);
+        Map<CommonCountProvider.CountBiz, Long> userIncMap = new HashMap<>();
+        for (CommonCountProvider.CountBiz countBiz : userCountBizList) {
+            userIncMap.put(countBiz, 1L);
         }
 
         Date occurTime = new Date();
         if (input.getOccur() != null) {
             occurTime = DateHelper.parseStringDate(input.getOccur(), DateHelper.DateFormatEnum.fullUntilSecond);
         }
+        String occurDate = DateHelper.dateToString(occurTime, DateHelper.DateFormatEnum.fullUntilDay);
+
+        Map<CommonCountProvider.CountBizDate, Long> pubIncMap = new HashMap<>();
+        for (CommonCountProvider.CountBiz countBiz : pubCountBizList) {
+            pubIncMap.put(countBiz.convertToBizDate(occurDate), 1L);
+        }
 
         Pair<MongoPersistEntity.PersistEntity, Map<CommonCountProvider.CountBiz, CommonCountProvider.CountBizEntity>> pair =
-                this.commonCountProvider.distributeSafeMultiBizCount(incMap, occurTime);
+                this.commonCountProvider.distributeSafeMultiBizCount(userIncMap, occurTime);
 
-        this.persistProvider.persist(Lists.newArrayList(pair.getLeft()));
+        MongoPersistEntity.PersistEntity pubPersistEntity = this.commonCountProvider.insertPersistHistory(pubIncMap);
+
+        this.persistProvider.persist(Lists.newArrayList(pair.getLeft(), pubPersistEntity));
 
         for (Map.Entry<CommonCountProvider.CountBiz, CommonCountProvider.CountBizEntity> entry : pair.getRight().entrySet()) {
             result.put(JsonHelper.writeValueAsString(entry.getKey()), entry.getValue().giveAfterAllTotal());
@@ -152,10 +170,33 @@ public class CommonCountController {
     }
 
 
+    private CommonCountProvider.CountBiz getBookFavoriteBiz(Long bookId, String action) {
+        CommonCountProvider.CountBiz data = new CommonCountProvider.CountBiz();
+
+        data.setTypeId(bookId.toString());
+        data.setBizType("book");
+        data.setSubBizType(action);
+
+        return data;
+    }
+
+    private CommonCountProvider.CountBiz getUserBookFavoriteBiz(UserContext userContext, Long bookId, String action) {
+        CommonCountProvider.CountBiz data = new CommonCountProvider.CountBiz();
+
+        data.setTypeId(userContext.getUserId().toString());
+        data.setBizType(String.format("%sBookTimes", action));
+        data.setSubBizType(bookId.toString());
+
+        return data;
+    }
+
+
     @Setter
     @Getter
-    public static class BizInput {
-        private List<CommonCountProvider.CountBiz> bizList = new ArrayList<>();
+    public static class BookInput {
+        private Set<Long> bookIds;
+
+        private String action;
 
         private List<String> dates = new ArrayList<>();
 
