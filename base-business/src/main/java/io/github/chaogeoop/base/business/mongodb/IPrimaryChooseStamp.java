f900f414fc530739c2eada2c598eff3b26a8b53e
lacmc <lacfg@163.com>
2024-04-22 17:14:50 +0800

    add

--- base-business/src/main/java/io/github/chaogeoop/base/business/mongodb/IPrimaryChooseStamp.java
+++ base-business/src/main/java/io/github/chaogeoop/base/business/mongodb/IPrimaryChooseStamp.java
@@ -2,8 +2,10 @@ package io.github.chaogeoop.base.business.mongodb;
 
 import io.github.chaogeoop.base.business.common.interfaces.IUserContext;
 
-public interface IPrimaryChooseStamp {
-    void record(IUserContext userContext, long stamp);
+import javax.annotation.Nullable;
 
-    Long read(IUserContext userContext);
+public interface IPrimaryChooseStamp {
+    void record(@Nullable IUserContext userContext, long stamp);
+
+    Long read(@Nullable IUserContext userContext);
 }
